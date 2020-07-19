1055b14e7b2cac04641c968844390879c97eaf24
Justin@Home <jalanmiddleton@gmail.com>
2020-07-18 23:14:22 -0400

    penalty part 2

--- quizzer/backend/routes/html-routes.js
+++ quizzer/backend/routes/html-routes.js
@@ -3,11 +3,7 @@ const mysql = require("mysql")
 module.exports = function(app, connection) {
   app.post("/", function(req, res) {
     let limit = req.body.songsper * req.body.playlists
-    let query =  `SELECT uri FROM (SELECT 
-        uri,
-        popularity,
-        MIN(idx),
-        COUNT(*),
+    let query =  `SELECT uri
         ${req.body.popularity} * popularity / 100 + 
           ${req.body.highest} * (101 - MIN(idx)) / 100 + 
           ${req.body.weeks} * COUNT(*) / 87 -
@@ -21,7 +17,7 @@ module.exports = function(app, connection) {
         uri IS NOT NULL and year(week) between ${req.body.start} and 
           ${req.body.end || req.body.start}
     GROUP BY id
-    ORDER BY points DESC limit ${limit}) tracks;`
+    ORDER BY points DESC limit ${limit};`
 
     connection.query(query, function(err, data) {
       (err)? res.send(err) : res.json({tracks: data.map(x => x.uri)})
