ce8e00629ae144a8c9d4c36c695f1e34e7757b36
Justin@Home <jalanmiddleton@gmail.com>
2020-07-18 23:14:06 -0400

    penalty

--- quizzer/backend/routes/html-routes.js
+++ quizzer/backend/routes/html-routes.js
@@ -10,11 +10,13 @@ module.exports = function(app, connection) {
         COUNT(*),
         ${req.body.popularity} * popularity / 100 + 
           ${req.body.highest} * (101 - MIN(idx)) / 100 + 
-          ${req.body.weeks} * COUNT(*) / 87 AS points
+          ${req.body.weeks} * COUNT(*) / 87 -
+          least(power(coalesce(tries, 0) / 5, 2), 1) AS points
     FROM
         billboard.tracks
             JOIN
         \`hot-100\` ON id = item_id
+        LEFT JOIN billboard.quiz on (id = songid)
     WHERE
         uri IS NOT NULL and year(week) between ${req.body.start} and 
           ${req.body.end || req.body.start}
@@ -27,20 +29,14 @@ module.exports = function(app, connection) {
   })
 
   app.post("/score", function(req, res) {
-    // let strs = "(" + 
-    //     req.body.songs.map(x => `${x.track.name} ${x.track.artists.map(y => y.name).join(",")}`).join(",") +
-    //     ")"
-    // strs = strs;
-    // let query = `UPDATE tracks SELECT id, uri FROM tracks limit 5;` // WHERE concat(spoffy_title, " ", spoffy_artist)`
-    
-    console.log(req.body)
     let addby = req.body.correct ? 1 : 0;
     let idquery = `INSERT INTO quiz
       SELECT id, now(), 1, ${addby}  FROM tracks WHERE uri IN
       (${req.body.songs.map(x => `"${x.track.uri}"`).join(",")})
       ON DUPLICATE KEY UPDATE	 
         tries = tries + 1,
-        correct = correct + ${addby}`
+        correct = case when ${addby} = 1 then correct + 1 else max(correct - 2, 0) end,
+        lastattempt = case when ${addby} = 1 then now() else lastattempt end`
     connection.query(idquery, function(err, data) {
       (err)? res.send(err) : res.json({ msg: "cool" })
     })
