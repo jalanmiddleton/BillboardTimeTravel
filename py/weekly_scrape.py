fd92075019f7bd9d1609c8cde02e815584a3d306
JustinAMiddleton <jalanmiddleton@gmail.com>
2022-07-10 11:50:15 -0400

    Remove unused import in scrape.

--- py/weekly_scrape.py
+++ py/weekly_scrape.py
@@ -1,10 +1,9 @@
 from BBtoDB import scrape
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 
 today = date.today()
 
 # 0 - 2, 1 - 3, 2 - 4...5 - 0, 6 - 1
 recentsaturday = today - timedelta((today.weekday() + 2) % 7)
 aweekago = recentsaturday - timedelta(days=7)
-
 scrape(recentsaturday, aweekago)
