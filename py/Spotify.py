f8a3740b931420f2fb778ada9ab9482ea8fbce0a
Justin's Home PC <jalanmiddleton@gmail.com>
2022-07-12 20:51:19 -0400

    BROKEN: Intermediate spotify changes.

--- py/Spotify.py
+++ py/Spotify.py
@@ -2,108 +2,103 @@
 Spotify wrapper.
 '''
 
+import re
+
 from secrets import secrets #pylint: disable=import-error,no-name-in-module
 
 import spotipy
-import spotipy.oauth2 as oauth2
+from spotipy import oauth2
 #import spotipy.util as util
 
 class Spotify:
-    __instance = None
+    '''
+    My custom class for the Spotify utilities.
+    '''
+    instance = None
+    __oauth = None
+    __token = None
 
     @staticmethod
     def get_instance():
-        if Spotify.__instance is None:
+        '''
+        Singleton method for this class.
+        '''
+        if (Spotify.instance is None or not Spotify.__oauth.validate_token(Spotify.__token)):
+            Spotify.instance = None
             Spotify()
-
-        if oauth2.is_token_expired(Spotify.__instance.__token):
-            self.__token = self.__oauth_credentials.get_access_token()
-            self.__sp = spotipy.Spotify(auth=self.__token,
-                                        oauth_manager=self.__oauth_credentials)
-
-        return Spotify.__instance.__sp
+        return Spotify.instance
 
     def __init__(self):
-        if Spotify.__instance is None:
-            self.__oauth_credentials = oauth2.SpotifyOAuth(
+        if Spotify.instance is None:
+            Spotify.__oauth = oauth2.SpotifyOAuth(
                 username=secrets['SPOTIFY_USER'],
                 client_id=secrets["SPOTIPY_CLIENT_ID"],
                 client_secret=secrets["SPOTIPY_SECRET"],
                 redirect_uri=secrets["SPOTIPY_REDIRECT_URI"],
                 scope='playlist-modify-public'
             )
+            Spotify.__token = Spotify.__oauth.get_access_token(as_dict=False)
+            Spotify.instance = spotipy.Spotify(auth=Spotify.__token,
+                                               oauth_manager=self.__oauth)
 
-            self.__token = self.__oauth_credentials.get_cached_token()
-            if not self.__token or oauth2.is_token_expired(self.__token):
-                self.__token = self.__oauth_credentials.get_access_token()
-            self.__sp = spotipy.Spotify(auth=self.__token["access_token"],
-                                        oauth_manager=self.__oauth_credentials)
+    @staticmethod
+    def search_item(item_type, title, artist):
+        title_bb = title.lower().strip()
+        artist_bb = artist.lower().strip() if artist else ""
+        query = get_query(title_bb, artist_bb)
+        search_results = Spotify.get_instance().search(q=query, type=item_type, limit=50)
 
-            Spotify.__instance = self
+        failed = []
+        for result in search_results[item_type + "s"]["items"]:
+            title_spoffy = remove_parens(result["name"].lower())
+            if has_bad_words(title_bb, title_spoffy, ["cover", "karaoke", "remix"]):
+                continue
 
-def remove_parens(s): return re.sub(r"\(.+\)|\[.+\]", "", s)
+            artist_spoffy = result["artists"][0]["name"].lower()
+            if has_bad_words(artist_bb, artist_spoffy, ["tribute", "karaoke"]):
+                break
 
+            if (LSSMatch(artist_bb, artist_spoffy) >= .75 or artist_bb == "soundtrack") \
+                and LSSMatch(title_spoffy, title_bb) >= .75:
+                return SpotifyItem(item_type, title, artist, search_result=result)
 
-def has_bad_words(original, result, words):
-    return any(word in original != word in result for word in words)
+            failed.append("\t\"%s\" by %s" % (title_spoffy, result["artists"][0]["name"]))
 
+        LOG("\t\"%s\" not found" % (query))
+        for fail in failed[:5]:
+            LOG("\t\t", fail)
 
-def get_query(title, artist):
-    artist = " ".join(filter(
-        lambda word: "feat" not in word
-        and word not in string.punctuation and len(word) > 1,
-        artist.lower().split()))
+        return SpotifyItem(item_type, title, artist)
 
-    return remove_parens(title) + " " + " ".join(re.split(r"\s|,", artist)[:3])
+    @staticmethod
+    def remove_parens(s): return re.sub(r"\(.+\)|\[.+\]", "", s)
 
-
-def search_item(item_type, title, artist):
-    title_bb = title.lower().strip()
-    artist_bb = artist.lower().strip() if artist else ""
-    query = get_query(title_bb, artist_bb)
-    search_results = Spotify.get_instance().search(q=query, type=item_type, limit=50)
-
-    failed = []
-    for result in search_results[item_type + "s"]["items"]:
-        title_spoffy = remove_parens(result["name"].lower())
-        if has_bad_words(title_bb, title_spoffy, ["cover", "karaoke", "remix"]):
-            continue
-
-        artist_spoffy = result["artists"][0]["name"].lower()
-        if has_bad_words(artist_bb, artist_spoffy, ["tribute", "karaoke"]):
-            break
-
-        if (LSSMatch(artist_bb, artist_spoffy) >= .75 or artist_bb == "soundtrack") \
-            and LSSMatch(title_spoffy, title_bb) >= .75:
-            return SpotifyItem(item_type, title, artist, search_result=result)
-
-        failed.append("\t\"%s\" by %s" % (title_spoffy, result["artists"][0]["name"]))
-
-    LOG("\t\"%s\" not found" % (query))
-    for fail in failed[:5]:
-        LOG("\t\t", fail)
-
-    return SpotifyItem(item_type, title, artist)
+    @staticmethod
+    def has_bad_words(original, result, words):
+        return any(word in original != word in result for word in words)
 
 
-def LSSMatch(one, two):
-    shortest, longest = (one, two) if len(one) < len(two) else (two, one)
+    @staticmethod
+    def LSSMatch(one, two):
+        shortest, longest = (one, two) if len(one) < len(two) else (two, one)
 
-    if len(shortest) == 0:
-        return 0
+        if len(shortest) == 0:
+            return 0
 
-    matrix = [[0 for i in range(len(shortest) + 1)]
-              for j in range(len(longest) + 1)]
-    for x in range(1, len(longest) + 1):
-        for y in range(1, len(shortest) + 1):
-            matrix[x][y] = (matrix[x - 1][y - 1] + 1) \
-                if longest[x - 1] == shortest[y - 1] \
-                else max(matrix[x - 1][y], matrix[x][y - 1])
+        matrix = [[0 for i in range(len(shortest) + 1)]
+                for j in range(len(longest) + 1)]
+        for x in range(1, len(longest) + 1):
+            for y in range(1, len(shortest) + 1):
+                matrix[x][y] = (matrix[x - 1][y - 1] + 1) \
+                    if longest[x - 1] == shortest[y - 1] \
+                    else max(matrix[x - 1][y], matrix[x][y - 1])
 
-    return float(matrix[-1][-1]) / len(shortest)
-
+        return float(matrix[-1][-1]) / len(shortest)
 
 class SpotifyItem:
+    '''
+    Encapsulation of an item to search for on Spotify.
+    '''
     def __init__(self, item_type, title, artist, search_result=None, uri=None):
         self.type = item_type
         self.details = {
@@ -151,3 +146,11 @@ class SpotifyItem:
 
     def __str__(self):
         return str(self.details)
+
+    def get_query(self, title, artist):
+        artist = " ".join(filter(
+            lambda word: "feat" not in word
+            and word not in string.punctuation and len(word) > 1,
+            artist.lower().split()))
+
+        return remove_parens(title) + " " + " ".join(re.split(r"\s|,", artist)[:3])
