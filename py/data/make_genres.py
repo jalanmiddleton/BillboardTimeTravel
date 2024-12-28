5b367449bfb7f2605c157e614d5c929e9dfe2f3b
jalanmiddleton <jalanmiddleton@gmail.com>
2024-12-28 12:18:28 -0500

    Make Genre List

---
+++ py/data/make_genres.py
@@ -0,0 +1,30 @@
+import csv
+
+import sys
+from pathlib import Path
+from pprint import pprint
+sys.path.append(str(Path(__file__).parent.parent))
+from spotify.Spotify import SpotifyItem
+
+uri_csv = "./py/data/songlinks.csv"
+genre_csv = "./py/data/songgenres.csv"
+
+with open(uri_csv, "r") as infile, open(genre_csv, "w", newline='') as outfile:
+   uri_reader = csv.reader(infile)
+   next(uri_reader)
+   genre_writer = csv.writer(outfile)
+   genre_writer.writerow(["title", "artist", "genres..."])
+   for *details, uri in uri_reader:
+      genres = SpotifyItem.from_uri(uri).get_genres() if uri else ['']
+      if not genres:
+         genres = ['']
+
+      row = [*details, *genres]
+      print(str(row))
+      genre_writer.writerow(row)
+      outfile.flush()
+
+
+# track = Spotify._get_instance().track(uri)
+# artist = Spotify._get_instance().artist(track['artists'][0]['uri'])
+# print(artist['genres'])
