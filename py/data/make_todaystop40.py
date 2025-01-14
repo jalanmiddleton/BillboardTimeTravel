ad5be81b43bf694f12e93fb87dfaf63c8dda104a
jalanmiddleton <jalanmiddleton@gmail.com>
2025-01-13 23:30:40 -0500

    Genre Playlist Generation

--- py/data/make_todaystop40.py
+++ py/data/make_todaystop40.py
@@ -8,13 +8,13 @@ import os.path as path
 sys.path.append("C:/Users/jalan/git/BillboardTimeTravel/py/spotify")
 from Spotify import Spotify
 
-def makeplaylists():
-    dirpath = "C:/Users/jalan/git/BillboardTimeTravel/"
+dirpath = "C:/Users/jalan/git/BillboardTimeTravel/"
+
+def get_todays_songs() -> dict[tuple, tuple]:
     song_csv = path.join(dirpath, "../rwd-billboard-data/data-out/hot-100-current.csv")
     score_csv = path.join(dirpath, "./py/data/top40score.csv")
     uri_csv = path.join(dirpath, "./py/data/songlinks.csv")
     adj_csv = path.join(dirpath, "./py/data/yearadjuster.csv")
-    debuts_csv = path.join(dirpath, "./py/data/songdebuts.csv")
 
     scores = {}
     with open(score_csv, "r") as score_infile:
@@ -38,32 +38,68 @@ def makeplaylists():
             adjusters[int(year)] = float(adj)
 
     today = datetime.date.today()
-    todays_songs = {}
+    todays_songs = []
     with open(song_csv, "r") as song_infile:
         song_reader = csv.reader(song_infile)
         next(song_infile)
         for day, _, title, artist, *_ in song_reader:
             day = datetime.date.fromisoformat(day)
-            key = (title, artist)
             if day.day == today.day and day.month == today.month:
-                todays_songs[key] = (scores.get(key, 0) * adjusters[day.year], uris.get(key, None))
+                todays_songs.append((title, 
+                                     artist, 
+                                     uris.get((title, artist), None), 
+                                     scores.get((title, artist), 0) * adjusters[day.year]))
 
-    todays_songs = sorted(todays_songs.items(), key=lambda item: item[1][0], reverse=True)
-    todays_songs = [x for x in todays_songs if x[1][1]][:100]  # if uri is not None
+    todays_songs = sorted(todays_songs, key=lambda item: item[3], reverse=True)
+    return todays_songs
+
+def make_genre_playlist():
+    todays_songs = get_todays_songs()
+
+    genre_csv = path.join(dirpath, "./py/data/songgenres.csv")
+    genres = {}
+    with open(genre_csv, "r") as genre_infile:
+        genre_reader = csv.reader(genre_infile)
+        next(genre_reader)
+        for song, artist, *gs in genre_reader:
+            genres[(song, artist)] = gs
+
+    todays_genres = {}
+    for song, artist, uri, _ in todays_songs:
+        for genre in genres[(song, artist)]:
+            if genre not in todays_genres:
+                todays_genres[genre] = []
+            if uri:
+                todays_genres[genre].append((song, artist, uri))
+    
+    top_genres = sorted([key for key in todays_genres.keys() if len(todays_genres[key]) >= 20 and len(todays_genres[key]) <= 60],
+                           key = lambda key: len(todays_genres[key]), 
+                           reverse=True)
+    
+    todays_genre: str = random.sample(top_genres, 1)[0]
+    todays_picks = [uri for *_, uri in random.sample(todays_genres[todays_genre], 20)]
+    Spotify.get_playlist("BB-Genre-.*").set_tracks(todays_picks)    
+    Spotify._get_instance().playlist_change_details(playlist_id=Spotify.get_playlist("BB-Genre-.*").id, name="BB-Genre-%s" % todays_genre.title())
+
+def makeplaylists():
+    today = datetime.date.today()
+
+    todays_songs = [song for song in get_todays_songs() if song[2]][:100]
 
     chosen_songs = []
-    random.seed(today.strftime("%Y-%m-%d    "))
+    random.seed(today.strftime("%Y-%m-%d"))
     chosen_songs.extend(random.sample(todays_songs[:30], 10))
     chosen_songs.extend(random.sample(todays_songs[30:60], 10))
     chosen_songs.extend(random.sample(todays_songs[60:80], 10))
     chosen_songs.extend(random.sample(todays_songs[80:100], 10))
-    chosen_songs = sorted(chosen_songs, key=lambda item: item[1][0], reverse=True)
-    chosen_songs = [uri for _, (_, uri) in chosen_songs]
+    chosen_songs = sorted(chosen_songs, key=lambda song: song[3], reverse=True)
+    chosen_songs = [uri for *_, uri, _ in chosen_songs]
 
     # All top ten
     Spotify.get_playlist("BB-Top40").set_tracks(chosen_songs)
 
-    debuts = {title_artist: None for title_artist, _ in todays_songs}
+    debuts_csv = path.join(dirpath, "./py/data/songdebuts.csv")
+    debuts = {(title, artist): None for title, artist, *_ in todays_songs}
     with open(debuts_csv, "r") as debuts_infile:
         debuts_reader = csv.reader(debuts_infile)
         next(debuts_reader)
@@ -71,8 +107,10 @@ def makeplaylists():
             title_artist = tuple(title_artist)
             if title_artist in debuts:
                 debuts[title_artist] = datetime.date.fromisoformat(debut)
-    todays_songs = sorted(todays_songs, key=lambda data: debuts[data[0]])
-    Spotify.get_playlist("BB-Hot100").set_tracks([uri for _, (_, uri) in todays_songs])
+    todays_songs = sorted(todays_songs, key=lambda song: debuts[(song[0], song[1])])
+    Spotify.get_playlist("BB-Hot100").set_tracks([uri for *_, uri, _ in todays_songs])
+
+    make_genre_playlist()
 
 if __name__ == "__main__":
     makeplaylists()
